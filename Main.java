ffef475c54045605ebfe769b614790f445c72b9c
Miguel Costa <126621837+miguelcostagap@users.noreply.github.com>
2023-08-24 00:26:54 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import Run.Game;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        Game game = new Game();
+        game.menuInit();
+    }
+}
